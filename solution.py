9ea6bc1fec50a0b439eeb2e1cd07c75b04c1b92c
PolinaRyu <80034164+PolinaRyu@users.noreply.github.com>
2023-04-07 23:42:06 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,5 +9,5 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    x = x*2/43/43
+    x = x*2/43/43 - stats.laplace.rvs(size=len(x))
     return x.mean() # Ваш ответ
